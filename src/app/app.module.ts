ddfa3beffe349b92a69e3fe3c0bd3af75eef20ad
Belkis <katerinne65@gmail.com>
2022-02-24 23:56:13 +0100

    Clase 145

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
@@ -7,8 +7,10 @@ import { SharedModule } from './shared/shared.module';
 import { AppRouterModule } from './app-router.module';
 import { VentasModule } from './ventas/ventas.module';
 
-
-
+//Cambiar el locale de la app
+import localesEs from '@angular/common/locales/es-HN';
+import { registerLocaleData } from '@angular/common';
+registerLocaleData(localesEs);
 
 @NgModule({
   declarations: [
@@ -20,7 +22,9 @@ import { VentasModule } from './ventas/ventas.module';
     SharedModule , 
     VentasModule   
   ],
-  providers: [],
+  providers: [
+    {provide: LOCALE_ID, useValue: 'es-HN'}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
