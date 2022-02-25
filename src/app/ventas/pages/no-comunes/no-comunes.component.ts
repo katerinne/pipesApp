fe40f2556054a12ab3cfd4309ca26b366d84d0dd
Belkis <katerinne65@gmail.com>
2022-02-25 23:55:00 +0100

    Clase 151

--- src/app/ventas/pages/no-comunes/no-comunes.component.ts
+++ src/app/ventas/pages/no-comunes/no-comunes.component.ts
@@ -7,11 +7,22 @@ import { Component } from '@angular/core';
   ]
 })
 export class NoComunesComponent{
+
+  //i18nSelect
   nombre: string = 'Susana';
   genero: string = 'femenino';
   invitacionMapa={
     'masculino': 'invitarlo',
     'femenino': 'invitarla'
+  }
+
+  //i18nPlural
+  clientes: string[] =['Pedro', 'Maria', 'Juan'];
+  clientesMapa = {
+    '=0': 'no tenemos nigún clientes esperando',
+    '=1': 'tenemos un cliente esperando',
+    '=2': 'tenemos 2 clientes esperando',
+    'other': 'tenemos # clientes esperando'
   }
 
 }
