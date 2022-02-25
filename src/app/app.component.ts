06f2d25d6a92d681b2e54ef0ec67210a165e356e
Belkis <katerinne65@gmail.com>
2022-02-25 01:23:23 +0100

    Clase 149

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PrimeNGConfig } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'pipesApp';
   nombre: string = 'fernando heRrera';
   valor: number = 1000;
@@ -17,4 +18,14 @@ export class AppComponent {
   mostrarNombre(){
     console.log(this.nombre);
   }
+
+  constructor(private primeNGConfit: PrimeNGConfig){}
+
+    ngOnInit() {
+      this.primeNGConfit.ripple=true;
+      
+    }
+  
 }
+
+
--- src/app/prime-ng/prime-ng.module.ts
+++ src/app/prime-ng/prime-ng.module.ts
@@ -4,18 +4,16 @@ import { NgModule } from '@angular/core';
 //primeNG
 import {ButtonModule} from 'primeng/button';
 import {CardModule} from 'primeng/card';
+import {FieldsetModule} from 'primeng/fieldset'
 import {MenubarModule} from 'primeng/menubar';
-import {MenuItem} from 'primeng/api';
-
-
 
 @NgModule({
   declarations: [],
   exports:[
     ButtonModule,
     CardModule,
-    MenubarModule,
-    
+    FieldsetModule,
+    MenubarModule    
   ]
 })
 export class PrimeNgModule { }
